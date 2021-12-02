91ccdd74a3a100507aa3a31087eeffda3ec7b537
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2021-12-02 13:32:00 +1030

    Fix Css issues with mobile nav bar, add about section

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,20 @@
 if($(window).width()>900){
-    $("nav").removeClass("navbar")
-    alert("hello")
+    $("nav").removeClass("overlay");
+    $("#navbarContent").removeClass("overlay-content");
+    $("span").remove();
+
+}else{
+    $("nav").remove();
 };
+
+
+
+function openNav() {
+    document.getElementById(
+        "myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById(
+        "myNav").style.width = "0%";
+}
