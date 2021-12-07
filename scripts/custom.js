cca9f26980b161def58aef066bdd9c3730456be7
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2021-12-07 16:38:20 +1030

    Add individual project boxes, add js to retrieve user click div

--- scripts/custom.js
+++ scripts/custom.js
@@ -7,14 +7,29 @@ if($(window).width()>900){
     $("nav").remove();
 };
 
-
-
 function openNav() {
-    document.getElementById(
-        "myNav").style.width = "100%";
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function openProjectInfo(){
+    document.getElementById("myProjects").style.width="100%";
+}
+
+function closeProjectInfo(){
+    document.getElementById("myProjects").style.width="0%";
 }
 
 function closeNav() {
-    document.getElementById(
-        "myNav").style.width = "0%";
+    document.getElementById("myNav").style.width = "0%";
 }
+
+//get id from divs that user clicks
+$("#projects div").click(function(e){
+    alert($(this).attr("id"));
+    return false;
+});
+
+$(".tagHolder div").click(function(f){
+    alert($(this).parent().parent().attr("id"));
+    return false;
+});
