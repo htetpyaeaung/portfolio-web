1ca2ee74a79d72d0355b32107fea880009c0778b
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2021-12-08 15:43:25 +1030

    Add dynamic content with js, remove angular usage

--- scripts/custom.js
+++ scripts/custom.js
@@ -9,6 +9,7 @@ if($(window).width()>900){
 
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
+    
 }
 
 function openProjectInfo(){
@@ -23,13 +24,36 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
+let mainValue;
+let tagValue;
+
 //get id from divs that user clicks
 $("#projects div").click(function(e){
-    alert($(this).attr("id"));
+    mainValue = $(this).attr("id");
+    jsonGet(mainValue);
     return false;
 });
 
 $(".tagHolder div").click(function(f){
-    alert($(this).parent().parent().attr("id"));
+    tagValue = $(this).parent().parent().attr("id");
+    jsonGet(tagValue);
     return false;
-});
+});   
+
+//get data from json file and add to individual project section tag
+function jsonGet(clickValue){
+    $.getJSON("project-data.json", function(data){
+        if(clickValue === undefined){
+            document.getElementById("p-heading").innerHTML=data[0].ProjectTitle;
+            document.getElementById("p-desc").innerHTML=data[0].Description;
+            document.getElementById("p-tech").innerHTML=data[0].Technologies;
+        }
+        else{
+            document.getElementById("p-heading").innerHTML=data[parseInt(mainValue.substring(1))-1].ProjectTitle;
+            document.getElementById("p-desc").innerHTML=data[parseInt(mainValue.substring(1))-1].Description;
+            document.getElementById("p-tech").innerHTML=data[parseInt(mainValue.substring(1))-1].Technologies;
+        }
+    }).fail(function(){
+        console.log("An error has occurred.");
+    });
+}
